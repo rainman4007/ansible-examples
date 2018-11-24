2eb014b4352eedae09bcade137012190ca6fb4f9
rain man <ansible@rainman40072.mylabserver.com>
2018-11-24 21:04:08 +0000

    updating custom vars

--- roles/webserver/library/custom_module.py
+++ roles/webserver/library/custom_module.py
@@ -2,8 +2,13 @@
 from ansible.module_utils.basic import *
 
 def another_function():
-  module = AnsibleModule(argument_spec={})
-  response = {"hello": "world"}
+   # define available arguments/parameters a user can pass to the module
+  module_args = dict( test_var=dict(type='str', required=True), new=dict(type='bool', required=False, default=False) )
+  # defining module to allow two parameters
+  module = AnsibleModule( argument_spec=module_args, supports_check_mode=True )
+  #module = AnsibleModule(argument_spec={})
+  response = {"test_var": module.params['test_var']}
+  #print("this is test var %s" % module.params['test_var'])
   module.exit_json(changed=False, meta=response)
 
 def custom_function():
